a433fe9234acf14f6f108affe040549c4a769448
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-01 15:14:22 -0500

    Returns List in JSON Format

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     // http://localhost:8080/student
@@ -11,5 +14,15 @@ public class StudentController {
     public Student getStudent() {
         Student student = new Student("Claudia","Ramirez", 1);
         return student;
+    }
+    // http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Tatiana", "Lopez", 1));
+        students.add(new Student("Paola", "Garcia", 2));
+        students.add(new Student("Maria", "Cueva", 3));
+        students.add(new Student("Gozde", "Salas", 4));
+        return students;
     }
 }
